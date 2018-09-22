74ecfa6909faa1076698e092a2eb6e5a667fd26f
satoshi ono <saongtx7@gmail.com>
2018-09-23 02:07:22 +0900

    Second Commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,7 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find_by(id: params[:id])
 	end
+	def index 
+		@users = User.where.not(id: current_user.id)
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   mount_uploader :img_name, ImgNameUploader
+  enum sex: {男: 0, 女: 1}
 end
 
