77de5c08e6c037398e37e2bc309128559b4b3e66
satoshi ono <saongtx7@gmail.com>
2018-09-23 02:58:30 +0900

    Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,9 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
   	users_path
   end
+  def after_sign_out_path_for(resource)
+    top_index_path
+  end
 end
 
 # %iは要素がシンボルの配列を作る
